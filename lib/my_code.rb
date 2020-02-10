f9c0a9e10904b0a12da5b3ae7b60b876c3831233
Michael Quigley <mikequigley@quigleypublishing.com>
2020-02-10 21:44:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,3 +9,19 @@ def map (source_array)
   end
   new_array
 end
+
+def reduce (source_array, starting_point = nil)
+  if starting_point
+    sum = starting_point
+    i = 0
+  else 
+    sum = array[0]
+    i = 1 
+  end 
+
+  while i < source_array.length 
+    sum = yield(sum, array[i])
+    i += 1 
+  end 
+  sum 
+end 

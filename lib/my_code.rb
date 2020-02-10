7c797347d23391fc278636739f2b99b3c25d65a1
Michael Quigley <mikequigley@quigleypublishing.com>
2020-02-10 21:47:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,7 +20,7 @@ def reduce (source_array, starting_point = nil)
   end
 
   while i < source_array.length
-    sum = yield(sum, source_array[i])
+    sum = yield(source_array[i], sum)
     i += 1
   end
   sum

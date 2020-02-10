a2a90e733a1fb9cc13204c887f05c7ecc79230e9
Michael Quigley <mikequigley@quigleypublishing.com>
2020-02-10 21:06:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,10 @@
 # Your Code Here
 
 def map (source_array)
-
-  yield
-
+  new_array = []
+  i = 0
+  while i < source_array.length
+    new_array << yield(source_array[i])
+  end
+  new_array
 end
